098ebf8c2c39f3a9e8cdf309f930dbcc573d5d51
boris-ns <b.sulicenko@gmail.com>
2017-04-17 14:36:05 +0200

    Uradjeno prepoznavanje mica i mogucnost da igraci jedu figure

--- main.py
+++ main.py
@@ -15,26 +15,41 @@ class Igra:
 
         return slobodna_polja
 
-    # Provera da li pozicija postoji i da li je slobodna
+    # Provera da li pozicija postoji
     def _proveri_poziciju(self, pozicija):
-        if (not 0 <= pozicija < 24) or (self._tabla[pozicija] != self._slobodno_polje):
-        #if not pozicija in self.nadji_slobodna_polja():
+        if not 0 <= pozicija < 24:
             return False
         
         return True
 
     # Zauzimanje polja na tabli
     def postavi_igraca(self, oznaka_igraca, pozicija):
-        if not self._proveri_poziciju(pozicija):
+        if (not self._proveri_poziciju(pozicija)) or (self._tabla[pozicija] != self._slobodno_polje):
             return False
 
         self._tabla[pozicija] = oznaka_igraca
         return True
 
+    # TODO: Pomeranje igraca
+    def pomeri_igraca(self, oznaka_igraca, pozicija):
+        pass
+
+    # Uklanjanje figure sa table. oznaka_igraca je od igraca koji jede
+    def ukloni_igraca(self, oznaka_igraca, pozicija):
+        if not self._proveri_poziciju(pozicija) or self._tabla[pozicija] == oznaka_igraca or self._tabla[pozicija] == self._slobodno_polje:
+            return False
+
+        if oznaka_igraca == self._igrac1.oznaka:
+            self._igrac2.broj_figura -= 1
+        else:
+            self._igrac1.broj_figura -= 1
+
+        self._tabla[pozicija] = self._slobodno_polje
+        return True
+
     # Iscrtavanje table na ekran
     def nacrtaj_tablu(self):
         # TODO: Probaj da ubacis i koordinate u ispis matrice, da korisnik ne mora napamet da trazi poziciju
-
         tabla = [
             [self._tabla[0], '-','-','-','-','-', self._tabla[1], '-','-','-','-','-', self._tabla[2]],
             ['|', ' ',' ',' ',' ',' ','|',' ',' ',' ',' ',' ','|'],
@@ -57,10 +72,19 @@ class Igra:
                 print(j, end="")
             print()
 
-    # Metoda za proveru da li je napravljena mica
-    # TODO:
-    def proveri_mica(self):
-        pass
+    
+    
+    # Metoda za proveru da li je napravljena mica za prosledjenog igraca
+    def proveri_micu(self, oznaka_igraca, poslednja_pozicija):
+        moguce_mice = ((0,1,2),(3,4,5),(6,7,8),(9,10,11),(12,13,14),(15,16,17),(18,19,20),(21,22,23),
+                       (0,9,21),(3,10,18),(6,11,15),(1,4,7),(16,19,22),(8,12,17),(5,13,20),(2,14,23))
+
+        for i, j, k in moguce_mice:
+            if i == poslednja_pozicija or j == poslednja_pozicija or k == poslednja_pozicija:
+                if self._tabla[i] == self._tabla[j] == self._tabla[k] and self._tabla[i] == oznaka_igraca:
+                    return True
+
+        return False
 
     # Provera da li je igra zavrsena. Igra je zavrsena ako je igrac ostao sa 2 figure
     # TODO: ili nema gde da se pomeri
@@ -85,13 +109,22 @@ class Igra:
 
         for potez in range(18):
             if potez % 2 == 0: # Beli igrac je na potezu
-                self._igrac1.postavi_figuru()
+                pozicija = self._igrac1.postavi_figuru()
+                if self.proveri_micu(self._igrac1.oznaka, pozicija):
+                    self._igrac1.pojedi_figuru()
+
             else:              # Crni igrac je na potezu
-                self._igrac2.postavi_figuru()
+                pozicija = self._igrac2.postavi_figuru()
+                if self.proveri_micu(self._igrac2.oznaka, pozicija):
+                    self._igrac2.pojedi_figuru()
 
             self.nacrtaj_tablu()
 
-        # TODO: poziv za FAZU 2
+        self.pomeraj_figure()
+
+    # FAZA 2: 
+    def pomeraj_figure(self):
+        pass
 
 class Covek:
     def __init__(self, oznaka, game_instance):
@@ -112,7 +145,7 @@ class Covek:
                 sys.exit()
             
             if self._game_instance.postavi_igraca(self.oznaka, pozicija):
-                break
+                return pozicija
 
             print("Ne mozete zauzeti polje ", pozicija)
 
@@ -120,7 +153,17 @@ class Covek:
         pass
 
     def pojedi_figuru(self):
-        pass
+        while True:
+            try:
+                pozicija = int(input("\n[{}] unesite polje da pojedete figuru: ".format(self.oznaka)))
+            except ValueError:
+                print("Uneli ste pogresnu vrednost!")
+                continue
+
+            if self._game_instance.ukloni_igraca(self.oznaka, pozicija):
+                break
+
+            print("Ne mozete ukloniti igraca na poziciji ", pozicija)
 
 if __name__ == "__main__":
     print("\n\n---===   MICE   ===---\n\n")
