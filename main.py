12f85c4da80e741012a759840c7b77d87bef2aa0
boris-ns <b.sulicenko@gmail.com>
2017-04-18 21:56:47 +0200

    Dodata mogucnost kretanja igraca po putanjama. Izmene i ulepsavanje koda.

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 class Igra:
+
+    _moguce_putanje = {
+        0 : (1, 9),
+        1 : (0, 2, 4),
+        2 : (1, 14),
+        3 : (4, 10),
+        4 : (1, 3, 5, 7),
+        5 : (4, 13),
+        6 : (7, 11),
+        7 : (4, 6, 8),
+        8 : (7, 12),
+        9 : (0, 10, 21),
+        10: (3, 9, 11, 18),
+        11: (6, 10, 15),
+        12: (8, 13, 17),
+        13: (5, 12, 14, 20),
+        14: (2, 13, 23),
+        15: (11, 16),
+        16: (15, 17, 19),
+        17: (12, 16),
+        18: (10, 19),
+        19: (16, 18, 22),
+        20: (13, 19),
+        21: (9, 22),
+        22: (19, 21, 23),
+        23: (14, 22)
+    }
+
+    _moguce_mice = ((0,1,2),(3,4,5),(6,7,8),(9,10,11),(12,13,14),(15,16,17),(18,19,20),(21,22,23),
+                       (0,9,21),(3,10,18),(6,11,15),(1,4,7),(16,19,22),(8,12,17),(5,13,20),(2,14,23))
+
     def __init__(self):
         self._slobodno_polje = 'X'
         self._tabla = [self._slobodno_polje for i in range(24)]
@@ -22,6 +53,44 @@ class Igra:
         
         return True
 
+    # Metoda za proveru da li je napravljena mica za prosledjenog igraca
+    def proveri_micu(self, oznaka_igraca, poslednja_pozicija):
+        for i, j, k in self._moguce_mice:
+            if i == poslednja_pozicija or j == poslednja_pozicija or k == poslednja_pozicija:
+                if self._tabla[i] == self._tabla[j] == self._tabla[k] and self._tabla[i] == oznaka_igraca:
+                    return True
+
+        return False
+
+    # Provera da li je neki od igaca ostao sa 3 figure. Tada je moguce skakanje za tog igraca.
+    # Ovo je i uslov za Fazu 3
+    def _proveri_mogucnost_skakanja(self):
+        if self._igrac1.broj_figura == 3 or self._igrac2.broj_figura == 3:
+            return True
+
+        return False
+
+    # Proveravanje da li figura sme da se krece po odredjenoj putanji
+    def _proveri_putanju_pomeranja(self, stara_poz, nova_poz):
+        if nova_poz in self._moguce_putanje[stara_poz]:
+            return True
+
+        return False
+
+    # Provera da li je igra zavrsena. Igra je zavrsena ako je igrac ostao sa 2 figure
+    # TODO: ili nema gde da se pomeri
+    def proveri_kraj_igre(self):
+        if self._igrac1.broj_figura == 2:
+            self._pobednik = self._igrac2.oznaka
+            return True
+        if self._igrac2.broj_figura == 2:
+            self._pobednik = self._igrac1.oznaka
+            return True
+        
+        # TODO: Proveri ako nema gde da se pomeri
+
+        return False
+
     # Zauzimanje polja na tabli
     def postavi_igraca(self, oznaka_igraca, pozicija):
         if (not self._proveri_poziciju(pozicija)) or (self._tabla[pozicija] != self._slobodno_polje):
@@ -31,15 +100,16 @@ class Igra:
         return True
 
     # Pomeranje igraca
-    # TODO: Naci sablon da se lakse prepozna da li se igrac sme kretati po odredjenoj putanji
     def pomeri_igraca(self, oznaka_igraca, stara_pozicija, nova_pozicija):
         uslov_pozicija = not self._proveri_poziciju(stara_pozicija) or not self._proveri_poziciju(nova_pozicija)
-        uslov_polje = self._tabla[stara_pozicija] != oznaka_igraca or self._tabla[nova_pozicija] != self._slobodno_polje
+        try: # TODO: nadji malo bolji nacin za roveru ako se unese neki bezveze indeks jer ovako dolazi do exceptiona
+            # a ruzno izgleda da se svi ovi uslovi stave u 1 if
+            uslov_polje = self._tabla[stara_pozicija] != oznaka_igraca or self._tabla[nova_pozicija] != self._slobodno_polje
+        except IndexError:
+            uslov_polje = False
 
-        if uslov_pozicija or uslov_polje:
+        if uslov_pozicija or uslov_polje or not self._proveri_putanju_pomeranja(stara_pozicija, nova_pozicija):
              return False
-
-        # TODO provera za putanju
 
         self._tabla[stara_pozicija] = self._slobodno_polje
         self._tabla[nova_pozicija] = oznaka_igraca
@@ -50,9 +120,16 @@ class Igra:
         if not self._proveri_poziciju(pozicija) or self._tabla[pozicija] == oznaka_igraca or self._tabla[pozicija] == self._slobodno_polje:
             return False
 
+        # TODO: Vidi da li mozes malo bolje da odredis oznaku drugog igraca
         if oznaka_igraca == self._igrac1.oznaka:
+            if self.proveri_micu(self._igrac2.oznaka, pozicija):
+                return False
+
             self._igrac2.broj_figura -= 1
         else:
+            if self.proveri_micu(self._igrac1.oznaka, pozicija):
+                return False
+                
             self._igrac1.broj_figura -= 1
 
         self._tabla[pozicija] = self._slobodno_polje
@@ -82,40 +159,18 @@ class Igra:
             for j in i:
                 print(j, end="")
             print()
+    
+    # Metode koje se pozivaju ukoliko je sastavljena mica za belog igraca
+    def _potez_beli(self, pozicija):
+        if self.proveri_micu(self._igrac1.oznaka, pozicija):
+            self.nacrtaj_tablu()
+            self._igrac1.pojedi_figuru()
 
-    # Metoda za proveru da li je napravljena mica za prosledjenog igraca
-    def proveri_micu(self, oznaka_igraca, poslednja_pozicija):
-        moguce_mice = ((0,1,2),(3,4,5),(6,7,8),(9,10,11),(12,13,14),(15,16,17),(18,19,20),(21,22,23),
-                       (0,9,21),(3,10,18),(6,11,15),(1,4,7),(16,19,22),(8,12,17),(5,13,20),(2,14,23))
-
-        for i, j, k in moguce_mice:
-            if i == poslednja_pozicija or j == poslednja_pozicija or k == poslednja_pozicija:
-                if self._tabla[i] == self._tabla[j] == self._tabla[k] and self._tabla[i] == oznaka_igraca:
-                    return True
-
-        return False
-
-    # Provera da li je igra zavrsena. Igra je zavrsena ako je igrac ostao sa 2 figure
-    # TODO: ili nema gde da se pomeri
-    def proveri_kraj_igre(self):
-        if self._igrac1.broj_figura == 2:
-            self._pobednik = self._igrac2.oznaka
-            return True
-        if self._igrac2.broj_figura == 2:
-            self._pobednik = self._igrac1.oznaka
-            return True
-        
-        # TODO: Proveri ako nema gde da se pomeri
-
-        return False
-
-    # Provera da li je neki od igaca ostao sa 3 figure. Tada je moguce skakanje za tog igraca.
-    # Ovo je i uslov za Fazu 3
-    def _proveri_mogucnost_skakanja(self):
-        if self._igrac1.broj_figura == 3 or self._igrac2.broj_figura == 3:
-            return True
-
-        return False
+    # Metode koje se pozivaju ukoliko je sastavljena mica za crnog igraca
+    def _potez_crni(self, pozicija):
+        if self.proveri_micu(self._igrac2.oznaka, pozicija):
+            self.nacrtaj_tablu()
+            self._igrac2.pojedi_figuru()
 
     # FAZA 1: Odavde pocinje igra. Postavljanje figura. Ova faza traje maksimalno 18 poteza.
     def postavi_figure(self, igrac1, igrac2):
@@ -127,12 +182,10 @@ class Igra:
         for potez in range(18):
             if potez % 2 == 0: # Beli igrac je na potezu
                 pozicija = self._igrac1.postavi_figuru()
-                if self.proveri_micu(self._igrac1.oznaka, pozicija):
-                    self._igrac1.pojedi_figuru()
+                self._potez_beli(pozicija)
             else:              # Crni igrac je na potezu
                 pozicija = self._igrac2.postavi_figuru()
-                if self.proveri_micu(self._igrac2.oznaka, pozicija):
-                    self._igrac2.pojedi_figuru()
+                self._potez_crni(pozicija)
 
             self.nacrtaj_tablu()
 
@@ -144,12 +197,10 @@ class Igra:
         while not self._proveri_mogucnost_skakanja():
             if potez % 2 == 0: # Beli igrac je na potezu
                 pozicija = self._igrac1.pomeri_figuru()
-                if self.proveri_micu(self._igrac1.oznaka, pozicija):
-                    self._igrac1.pojedi_figuru()
+                self._potez_beli(pozicija)
             else:              # Crni igrac je na potezu
                 pozicija = self._igrac2.pomeri_figuru()
-                if self.proveri_micu(self._igrac2.oznaka, pozicija):
-                    self._igrac2.pojedi_figuru()
+                self._potez_crni(pozicija)
 
             self.nacrtaj_tablu()
             potez += 1
